5bc371feced18b874a3f5311d018942cb403eff2
Tarik Horozovic <thorozovic1@etf.unsa.ba>
2020-10-30 17:52:01 +0100

    Implementirane osnovne metode u klasama Artikl, Korpa i Supermarket

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,38 @@
 package ba.unsa.etf.rpr;
 
+import java.util.ArrayList;
+
 public class Korpa {
+    private ArrayList<Artikl> kupljeno = new ArrayList<Artikl>();
+    public boolean dodajArtikl(Artikl a) {
+        if (kupljeno.size()>=50)
+            return false;
+        kupljeno.add(a);
+        return true;
+    }
+
+    public Artikl[] getArtikli() {
+        Artikl[] povratni = new Artikl[kupljeno.size()];
+        povratni = kupljeno.toArray(povratni);
+        return povratni;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        Artikl izbaci = null;
+        for(Artikl a : kupljeno)
+        {
+            if (kod.equals(a.getKod()))
+                izbaci = a;
+        }
+        if (izbaci != null)
+            kupljeno.remove(izbaci);
+        return izbaci;
+    }
+
+    public int dajUkupnuCijenuArtikala() {
+        int cijena = 0;
+        for (Artikl item : kupljeno)
+            cijena+=item.getCijena();
+        return cijena;
+    }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,29 @@
 package ba.unsa.etf.rpr;
 
+import java.util.ArrayList;
+
 public class Supermarket {
+    private ArrayList<Artikl> inventar = new ArrayList<Artikl>();
+
+    public void dodajArtikl(Artikl artikl){
+        inventar.add(artikl);
+    }
+
+    public Artikl[] getArtikli() {
+        Artikl[] povratni = new Artikl[inventar.size()];
+        povratni = inventar.toArray(povratni);
+        return povratni;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        Artikl izbaci = null;
+        for(Artikl a : inventar)
+        {
+            if (kod.equals(a.getKod()))
+                izbaci = a;
+        }
+        if (izbaci != null)
+            inventar.remove(izbaci);
+        return izbaci;
+    }
 }
