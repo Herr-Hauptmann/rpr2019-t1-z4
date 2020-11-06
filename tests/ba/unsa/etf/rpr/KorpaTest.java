c44f0575c4900ef4cc18050d1aa1e424b34b43f9
Tarik Horozovic <thorozovic1@etf.unsa.ba>
2020-11-06 16:53:00 +0100

    Dodana dva testa za klasu Korpa

--- tests/ba/unsa/etf/rpr/KorpaTest.java
+++ tests/ba/unsa/etf/rpr/KorpaTest.java
@@ -8,17 +8,28 @@ class KorpaTest {
 
     @Test
     void dodajArtikl() {
+        Artikl artikl = new Artikl("Avion", 1800, "1");
+        Korpa korpa = new Korpa();
+        assertTrue(korpa.dodajArtikl(artikl));
     }
 
     @Test
     void getArtikli() {
+        Artikl artikl1 = new Artikl("Avion", 1800, "1");
+        Artikl artikl2 = new Artikl("Auto", 1200, "2");
+        Artikl artikl3 = new Artikl("Avion", 1000, "3");
+        Korpa korpa = new Korpa();
+        korpa.dodajArtikl(artikl1);
+        korpa.dodajArtikl(artikl2);
+        korpa.dodajArtikl(artikl3);
+        assertEquals(artikl2.getCijena(), korpa.getArtikli()[1].getCijena());
     }
 
-    @Test
-    void izbaciArtiklSaKodom() {
-    }
-
-    @Test
-    void dajUkupnuCijenuArtikala() {
-    }
+//    @Test
+//    void izbaciArtiklSaKodom() {
+//    }
+//
+//    @Test
+//    void dajUkupnuCijenuArtikala() {
+//    }
 }
